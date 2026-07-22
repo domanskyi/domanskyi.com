055b3f5d6caa89c2c59f7df1f623ab5ca905255f
domanskyi <bruce3773@gmail.com>
2026-07-22 22:18:56 +0200

    dna

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { Contacts } from "./components/contacts";
 import { Socials } from "./components/socials";
 import Link from "next/link";
 import { CaretRightIcon } from "@phosphor-icons/react/dist/ssr";
+import Image from "next/image";
+import domanskyiPhoto from "../public/img/domanskyi.webp";
 
 export default async function Page() {
   const blogPosts = getBlogPosts(5);
@@ -59,6 +61,16 @@ export default async function Page() {
         />
       </div>
       <div className="w-full h-[.0625rem] bg-neutral-200 mb-8" />
+      {/* <Link
+        target="_blank"
+        href="https://science.nasa.gov/specials/your-name-in-landsat/?img1=d_0&img2=o_1&img3=m_2&img4=a_4&img5=n_0&img6=s_1&img7=k_0&img8=y_1&img9=i_4"
+      >
+        <Image
+          src={domanskyiPhoto}
+          alt="NASA"
+          className="h-auto w-full rounded-md"
+        />
+      </Link> */}
     </section>
   );
 }
